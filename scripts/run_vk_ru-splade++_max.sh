0ada798d171ef0581017ac9468ca8c1f1f5f3f65
lesha <grishchenkoalexey2004@yandex.ru>
2025-04-23 14:25:16 +0300

    Added debug check in launch scripts

--- scripts/run_vk_ru-splade++_max.sh
+++ scripts/run_vk_ru-splade++_max.sh
@@ -2,4 +2,13 @@
 # splade based on ai-forever/ruSbert
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 export SPLADE_CONFIG_NAME="config_vk_ru-splade++_max.yaml"
-python -m splade.train
+
+if [ "${DEBUG_SPLADE}" = "1" ]; then
+    python -m pdb -m splade.train
+    exit 0
+fi
+
+if [ -z "${DEBUG_SPLADE}" ] || [ "${DEBUG_SPLADE}" = "0" ]; then
+    python -m splade.train
+    exit 0
+fi
