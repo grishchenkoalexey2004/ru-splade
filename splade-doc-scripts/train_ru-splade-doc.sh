b88132dc1f339797b9532f14826a513690e0bcfe
lesha <grishchenkoalexey2004@yandex.ru>
2025-04-28 20:54:09 +0300

    Fixed train script

--- splade-doc-scripts/train_ru-splade-doc.sh
+++ splade-doc-scripts/train_ru-splade-doc.sh
@@ -69,18 +69,16 @@ fi
 
 
 
-if [ "${DEBUG_SPLADE}" = "0" ]; then
-    python -m splade.train \
-        +config.checkpoint_dir=$CHECKPOINT_DIR \
-        +config.index_dir=$INDEX_DIR \
-        +config.out_dir=$OUT_DIR \
-        config.regularizer.FLOPS.lambda_q=0.0000 \
-        config.regularizer.FLOPS.lambda_d=$LAMBDA_D \
-        +config.random_seed=100 \
-        init_dict.model_type_or_dir=$MODEL_NAME \
-        config.tokenizer_type=$MODEL_NAME
-    exit 0
-fi
+python -m splade.train \
+    +config.checkpoint_dir=$CHECKPOINT_DIR \
+    +config.index_dir=$INDEX_DIR \
+    +config.out_dir=$OUT_DIR \
+    config.regularizer.FLOPS.lambda_q=0.0000 \
+    config.regularizer.FLOPS.lambda_d=$LAMBDA_D \
+    +config.random_seed=100 \
+    init_dict.model_type_or_dir=$MODEL_NAME \
+    config.tokenizer_type=$MODEL_NAME
+exit 0
 
 
 
